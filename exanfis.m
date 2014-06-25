094cec517bb2c16c07cf64cce58363839af90737
lepisma <abhinav.tushar.vs@gmail.com>
2014-06-26 02:15:13 +0530

    Change function to use matlab fuzzy logic tool

--- exanfis.m
+++ exanfis.m
@@ -1,184 +1,55 @@
-function accu = exanfis(x_train, y_train, x_test, y_test, n_mfs)
+function fis = exanfis(x_train, y_train, n_mfs)
     
     %----------------------------------------------------------------------
     % TRAINING
+    
+    % Generating FIS
+    % `genfis1` needs data so that the last column in matrix is the output
+    % while all columns before are inputs.
+    % So we need to merge x and y
     [n_observations, n_variables] = size(x_train);
     
-    % Initializing gbell membership parameters.
-    % This matrix holds the whole randomised membership functions
-    % parameters `a`, `b` and `c`
-    % The choice of limits of range of randomisation is made as per the
-    % paper "Comparison of Extrem ANFIS and ANFIS for regression problems"
-    mf_params = zeros(n_mfs * n_variables, 3);
+    data = [x_train, y_train'];
+    mf_type = 'gbellmf';
     
-    % Setting parameter `b` in the range of 1.9 to 2.1
-    mf_params(:, 2) = 2.1 - (0.2 * rand(n_mfs * n_variables, 1));
+    fis = genfis1(data, n_mfs, mf_type);
     
-    % Settings `a` and `c` for each attribute in dataset
-    for i = 1:n_variables
-        range = range(x_train(:, i));
-        
-        % Setting `a` in range of (0.5 * tmp_a) to (1.5 * tmp_a))
-        tmp_a = range / (2 * (n_mfs - 1));
-        mf_params((i - 1) * n_mfs + 1 : (i * n_mfs), 1) = tmp_a * (1.5 - rand(n_mfs, 1));
-        
-        % Setting `c`
-        tmp_c = linspace(min(x_train(:, i)), max(x_train(:, i)), n_mfs);
-        diff_c = (max(x_train(:, i)) - min(x_train(:, i))) / (n_mfs - 1);
-        
-        mf_params((i - 1) * n_mfs + 1, 3) = tmp_c(1) + (diff_c / 2) * rand();
-        mf_params((i - 1) * n_mfs + 2 : (i * n_mfs) - 1, 3) = tmp_c(2 : end - 1) + (diff_c / 2) * (1 - 2 * rand());
-        mf_params((i * n_mfs), 3) = tmp_c(end) - (diff_c / 2) * rand();
-    end
+    % Now we have all rules made, and membership functions uniformly
+    % distributed.
+    % We have to think about randomising membership functions, but they are
+    % not a problem as of now.
+    % Currently, all the consequence parameters are set to 0. So `evalfis`
+    % at any input will give 0
+    % But we are not interested in that, `evalfis` also returns a value of
+    % firing strengths of all the rules. We are going to find them for each
+    % input instance and then calculate the `H` matrix as in `exanfis`
+    % function.
     
-    clear tmp_a;
-    clear tmp_c;
-    clear diff_c;
-    
-    % Finding firing of all membership functions
-    % `firing_strengths` is a matrix with each row representing single
-    % observation and values of columns represent firing strengths of all
-    % membership functions for all input variables
-    firing_strengths = zeros(n_observations, n_mfs * n_variables);
-    
-    for i = 1:n_observations
-        for j = 1:n_variables
-            for k = 1:n_mfs
-                tmp_fire = gbellmf(x_train(i, j), mf_params((j - 1) * n_mfs + k, :));
-            end
-            firing_strengths(i, (j - 1) * n_mfs + 1 : (j * n_mfs)) = tmp_fire;
-        end
-    end
-    
-    clear tmp_fire;
-    
-    n_rules = n_mfs ^ n_variables;
-    
-    % Making rules considering all possible combinations
-    % Finding their firing strengths by taking minimum
-    rule = n_mfs * (1 : n_variables) - (n_mfs - 1);
-    % rule here is a list of elements to be chosen from firing_strengths to
-    % form a rule
-    rule_add = [zeros(1, n_variables), 1];
-    
-    count = 1;
-    
-    rule_firings = zeros(n_observations, n_rules);
-    
-    for i = 1:n_observations
-        for j = 1:n_rules
-            rule_firings(i, j) = min(firing_strengths(i, rule));
-            if count < n_mfs
-                rule = rule + rule_add;
-                count = count + 1;
-            else
-                count = 1;
-                rule_add = circshift(rule_add', 1)';
-            end
-        end
-    end
-    
-    clear firing_strengths;
-    
-    % Making H matrix
-    
-    x_new = [x_train', ones(1, n_observations)];
-    
+    n_rules = size(fis.rule, 2);
     H = zeros(n_rules * (n_variables + 1), n_observations);
-    tmp_h = zeros(n_variables + 1, n_observations);
     
-    for i = 1:n_rules
-        for j = 1:n_observations
-            tmp_h(:, j) = x_new(:, j) * rule_firings(j, i);
-        end
+    % For each input instance
+    for i = 1:n_observations
+        % Finding firings
+        [~, IRR] = evalfis(x_train(i, :), fis);
+        rule_firings = min(IRR, [], 2);
         
-        H((i - 1) * (n_variables + 1) + 1 : i * (n_variables + 1), :) = tmp_h;
+        tmp_in = repmat([x_train(i, :) 1]', [n_rules, 1]);
+        tmp_wt = repmat(rule_firings, [1, n_variables + 1]);
+        
+        h_col = tmp_in .* tmp_wt;
+        h_col = h_col';
+        
+        H(:, i) = h_col(:);
     end
     
-    clear x_new;
-    clear tmp_h;
-    clear rule_firings;
-    
-    % H generated
-    % now parameter matrix can be represented as y * pinv(H)
-    
+    % ELM thing
     P = y_train * pinv(H);
+    P = reshape(P, [n_variables + 1, n_rules])';
     
-    % Training complete
-    
-    %----------------------------------------------------------------------
-    % TESTING
-    
-    n_observations_test = size(x_test, 1);
-    
-    % Finding firing strengths for membership functions using the earlier
-    % generated `mf_params`
-    firing_strengths_test = zeros(n_observations_test, n_mfs * n_variables);
-    
-    for i = 1:n_observations_test
-        for j = 1:n_variables
-            for k = 1:n_mfs
-                tmp_fire = gbellmf(x_test(i, j), mf_params((j - 1) * n_mfs + k, :));
-            end
-            firing_strengths_test(i, (j - 1) * n_mfs + 1 : (j * n_mfs)) = tmp_fire;
-        end
-    end
-    
-    clear tmp_fire;
-    
-    % Find firings for rules
-    % Same rules should be here, implying that rules have to be stored in
-    % some form.
-    
-    rule = n_mfs * (1 : n_variables) - (n_mfs - 1);
-    rule_add = [zeros(1, n_variables), 1];
-    
-    count = 1;
-    
-    rule_firings_test = zeros(n_observations_test, n_rules);
-    
-    for i = 1:n_observations_test
-        for j = 1:n_rules
-            rule_firings_test(i, j) = min(firing_strengths_test(i, rule));
-            if count < n_mfs
-                rule = rule + rule_add;
-                count = count + 1;
-            else
-                count = 0;
-                rule_add = circshift(rule_add', 1)';
-            end
-        end
-    end
-    
-    clear count;
-    clear firing_strengths_test;
-    clear rule;
-    clear rule_add;
-    
-    % Rule firings found
-    % Finding H
-    
-    x_new_test = [x_test', ones(1, n_observations_test)];
-    
-    H = zeros(n_rules * (n_variables_test + 1), n_observations_test);
-    tmp_h = zeros(n_variables_test + 1, n_observations_test);
-    
+    % Inserting the values of calculated parameters in generated fis
     for i = 1:n_rules
-        for j = 1:n_observations_test
-            tmp_h(:, j) = x_new_test(:, j) * rule_firings_test(j, i);
-        end
-        
-        H((i - 1) * (n_variables_test + 1) + 1 : i * (n_variables_test + 1), :) = tmp_h;
+        fis.output.mf(i).params = P(i, :);
     end
     
-    clear rule_firings_test;
-    clear tmp_h;
-    clear x_new_test;
-    
-    % H found, finding output
-    
-    output = P * H;
-    
-    % Checking for accuracy
-    accu = mean(output == y_test);
 end
