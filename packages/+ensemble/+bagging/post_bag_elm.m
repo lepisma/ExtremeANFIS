69fdea56132c54aa9dcf278f5397664d89d7a7e6
lepisma <abhinav.tushar.vs@gmail.com>
2014-07-11 05:15:34 +0530

    Add bagging weights finder

---
+++ packages/+ensemble/+bagging/post_bag_elm.m
@@ -0,0 +1,20 @@
+function weights = post_bag_elm(fisses, data_train)
+
+% Finds the weights to be used for finding final output
+
+n_fis = size(fisses);
+weights = zeros(1, n_fis);
+
+n_observations = size(data_train, 1);
+
+output_matrix = zeros(n_observations, n_fis);
+
+for i = 1 : n_fis
+    
+    output_matrix(:, i) = evalfismex(data_test(:, 1 : end - 1), fisses{i}, 101);
+    
+end
+
+weights = pinv(output_matrix) * data_test(:, end);
+
+end
